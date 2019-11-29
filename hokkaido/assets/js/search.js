880dc8ef57368a1a813b981f5fae699a4321aff7
nak-244 <t09024656860@gmail.com>
2019-11-29 11:14:48 +0900

    up

--- hokkaido/assets/js/search.js
+++ hokkaido/assets/js/search.js
@@ -1,9 +1,8 @@
 $(function() {
   var href = "https://sigotora.jp/index.cfm";
-  $('#srh_ken_param,#srh_jobtype_param,#sfw').on('change', function() {
-    var srh_ken_param = $('#srh_ken_param').val();
+  $('#srh_jobtype_param,#sfw').on('change', function() {
     var srh_jobtype_param = $('#srh_jobtype_param').val();
     var sfw = $('#sfw').val();
-    $('#test').prop('href', href + "?fuseaction=job.joblist&srh_ken_param=" + srh_ken_param + "&srh_jobtype_param=" + srh_jobtype_param + "&sfw=" + sfw);
+    $('#test').prop('href', href + "?fuseaction=job.joblist&srh_area_param=1" + "&srh_jobtype_param=" + srh_jobtype_param + "&sfw=" + sfw);
   });
 });
