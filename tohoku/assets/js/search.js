abb3d7a0acdfc8c1b7a1f702731ed45bc2bcb02b
nak-244 <t09024656860@gmail.com>
2019-12-09 12:30:17 +0900

    up

--- tohoku/assets/js/search.js
+++ tohoku/assets/js/search.js
@@ -3,6 +3,6 @@ $(function() {
   $('#srh_jobtype_param,#sfw').on('change', function() {
     var srh_jobtype_param = $('#srh_jobtype_param').val();
     var sfw = $('#sfw').val();
-    $('#test').prop('href', href + "?fuseaction=job.joblist&srh_area_param=1" + "&srh_jobtype_param=" + srh_jobtype_param + "&sfw=" + sfw);
+    $('#test').prop('href', href + "?fuseaction=job.joblist&srh_area_param=2" + "&srh_jobtype_param=" + srh_jobtype_param + "&sfw=" + sfw);
   });
 });
